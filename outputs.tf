32f2e60255afe2e817484910cbf6d0f7b67d4fb7
Brent Gawryluik <brentwg@zoho.com>
2018-02-21 11:51:13 -0800

    Removed output - key fingerprint

--- outputs.tf
+++ outputs.tf
@@ -8,8 +8,14 @@ output "key_pair_name" {
   value       = "${aws_key_pair.this.key_name}"
 }
 
+/*
+- as of testing 2018-02-21, this output is no longer working
+- can't figure out exactly why
+- decided it's uncessary, removing
+- code remains for posterity
 # SSH Key Finger Print
 output "key_pair_fingerprint" {
   description = "The MD5 public key fingerprint"
   value       = "${aws_key_pair.this.fingerprint}"
 }
+*/
